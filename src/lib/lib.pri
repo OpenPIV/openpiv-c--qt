9fdcbde22fba94be3099f7472b8ca8f6c9fb8b8e
Tim Dewhirst <tim@bugless.co.uk>
2018-03-22 07:25:08 +0000

    tidyup prior to restructure

--- src/lib/lib.pri
+++ src/lib/lib.pri
@@ -2,6 +2,7 @@
 
 include( interfaces/interfaces.pri )
 include( core/core.pri )
+include( util/util.pri )
 
 
 
--- src/lib/util/point.h
+++ src/lib/util/point.h
@@ -1,6 +1,10 @@
 
 #pragma once
 
+// std
+#include <cstdint>
+#include <type_traits>
+
 /// basic 3D point
 template < typename T >
 class Point
@@ -10,12 +14,27 @@ public:
     Point( const Point& ) = default;
     Point( Point&& ) = default;
 
+    template < typename U,
+               typename = std::enable_if<std::is_convertible<U, T>::value>::type >
+    Point( const Point< U >& p )
+        : x_( p.x_ ),
+          y_( p.y_ ),
+          z_( p.z_ )
+    {}
     
-
+    const T& x() const { return x_; }
+    const T& y() const { return y_; }
+    const T& z() const { return z_; }
     
-
 private:
     T x_ = {};
     T y_ = {};
     T z_ = {};
 };
+
+
+/// specialization for integer
+using IntPoint = Point< uint32_t >;
+
+/// specialization for doubles
+using DoublePoint = Point< double >;
