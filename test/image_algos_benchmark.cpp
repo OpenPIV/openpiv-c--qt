a0f0c8220c5002b97b889f38166c0b00f9136a38
timdewhirst <tim@bugless.co.uk>
2021-04-05 16:48:02 +0100

    fix benchmark source images, test for view v's extract

--- test/image_algos_benchmark.cpp
+++ test/image_algos_benchmark.cpp
@@ -12,48 +12,78 @@
 using namespace openpiv::core;
 using namespace openpiv::algos;
 
-static void fft_cross_correlation_benchmark(benchmark::State& state)
+static void fft_cross_correlation_view_benchmark(benchmark::State& state)
 {
     cf_image im_a{ load_from_file< g_f >( "corr_a.tiff" ) };
-    cf_image im_b{ load_from_file< g_f >( "corr_b.tiff" ) };
     uint32_t d{ (uint32_t)state.range(0) };
     size s{ d, d };
+    FFT fft( s );
 
-    auto view_a = create_image_view( im_a, rect{ {}, s } );
-    auto view_b = create_image_view( im_b, rect{ {}, s } );
+    auto view_a = create_image_view( im_a, rect{ {0, 0}, s } );
+    auto view_b = create_image_view( im_a, rect{ {1, 1}, s } );
 
     for (auto _ : state)
     {
-        state.PauseTiming();
-        FFT fft( s );
-        state.ResumeTiming();
-
         // measure FFT speed
         fft.cross_correlate( view_a, view_b );
     }
 }
 // Register the function as a benchmark
-BENCHMARK(fft_cross_correlation_benchmark)->Threads(2)->RangeMultiplier(2)->Range(2, 64);
+BENCHMARK(fft_cross_correlation_view_benchmark)->Threads(4)->RangeMultiplier(2)->Range(4, 64);
 
-static void fft_auto_correlation_benchmark(benchmark::State& state)
+static void fft_cross_correlation_extract_benchmark(benchmark::State& state)
 {
     cf_image im_a{ load_from_file< g_f >( "corr_a.tiff" ) };
     uint32_t d{ (uint32_t)state.range(0) };
     size s{ d, d };
+    FFT fft( s );
+
+    auto sub_a = extract( im_a, rect{ {0, 0}, s } );
+    auto sub_b = extract( im_a, rect{ {1, 1}, s } );
+
+    for (auto _ : state)
+    {
+        // measure FFT speed
+        fft.cross_correlate( sub_a, sub_b );
+    }
+}
+// Register the function as a benchmark
+BENCHMARK(fft_cross_correlation_extract_benchmark)->Threads(4)->RangeMultiplier(2)->Range(4, 64);
+
+static void fft_auto_correlation_view_benchmark(benchmark::State& state)
+{
+    cf_image im_a{ load_from_file< g_f >( "corr_a.tiff" ) };
+    uint32_t d{ (uint32_t)state.range(0) };
+    size s{ d, d };
+    FFT fft( s );
 
     auto view_a = create_image_view( im_a, rect{ {}, s } );
 
     for (auto _ : state)
     {
-        state.PauseTiming();
-        FFT fft( s );
-        state.ResumeTiming();
-
         // measure FFT speed
         fft.auto_correlate( view_a );
     }
 }
 // Register the function as a benchmark
-BENCHMARK(fft_auto_correlation_benchmark)->Threads(2)->RangeMultiplier(2)->Range(2, 64);
+BENCHMARK(fft_auto_correlation_view_benchmark)->Threads(4)->RangeMultiplier(2)->Range(4, 64);
+
+static void fft_auto_correlation_extract_benchmark(benchmark::State& state)
+{
+    cf_image im_a{ load_from_file< g_f >( "corr_a.tiff" ) };
+    uint32_t d{ (uint32_t)state.range(0) };
+    size s{ d, d };
+    FFT fft( s );
+
+    auto view_a = extract( im_a, rect{ {}, s } );
+
+    for (auto _ : state)
+    {
+        // measure FFT speed
+        fft.auto_correlate( view_a );
+    }
+}
+// Register the function as a benchmark
+BENCHMARK(fft_auto_correlation_extract_benchmark)->Threads(4)->RangeMultiplier(2)->Range(4, 64);
 
 BENCHMARK_MAIN();
