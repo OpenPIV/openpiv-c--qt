06c30d27c6db1894ef953814a3bb9cfb936805af
timdewhirst <tim@bugless.co.uk>
2021-12-31 16:29:07 +0000

    remove travis scripts

--- .ci/windows-upgrade-to-vs2019.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-if [ "${TRAVIS_OS_NAME}" = "windows" ]; then
-  choco install visualstudio2019buildtools --package-parameters \
-    "--add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Component.Windows10SDK"
-  choco upgrade cmake
-fi
