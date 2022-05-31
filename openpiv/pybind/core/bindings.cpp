2b5be7de65c681aa9d235c4ed0a525d3de93f76f
Tim Dewhirst <tim@bugless.co.uk>
2022-05-31 10:00:02 +0100

    WIP pybind11 PoC #15

--- openpiv/pybind/core/bindings.cpp
+++ openpiv/pybind/core/bindings.cpp
@@ -5,8 +5,13 @@
 using namespace openpiv;
 using namespace openpiv::core;
 
-// pybiond
+// pybind
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/operators.h>
+
+// std
+#include <sstream>
 
 namespace py = pybind11;
 
@@ -25,9 +30,25 @@ PYBIND11_MODULE(pyopenpivcore, m) {
 
     // size
     py::class_<size>(m, "size")
+        .def(py::init())
         .def(py::init<size::component_t, size::component_t>())
         .def("width", &size::width)
-        .def("height", &size::height);
+        .def("height", &size::height)
+        .def("area", &size::area)
+        .def("components", &size::components)
+        .def(py::self += py::self)
+        .def(py::self + py::self)
+        .def(py::self -= py::self)
+        .def(py::self - py::self)
+        .def("__repr__",
+             [](const size& s) {
+                 std::ostringstream ss;
+                 ss << s;
+                 return ss.str();
+             });
+    m.def("maximal_size", &maximal_size);
+    m.def("minimal_size", &minimal_size);
+    m.def("transpose", &transpose);
 
 #ifdef VERSION_INFO
     m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
