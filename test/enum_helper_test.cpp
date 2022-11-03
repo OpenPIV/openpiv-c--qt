a73acc07dffcfc0db69e55ce8ca39095fa41678c
Tim Dewhirst <tim@bugless.co.uk>
2022-11-03 00:17:54 +0000

    extend enum_helper to allow parsing of enums

--- test/enum_helper_test.cpp
+++ test/enum_helper_test.cpp
@@ -4,6 +4,7 @@
 
 // std
 #include <sstream>
+#include <tuple>
 
 // local
 #include "test_utils.h"
@@ -31,6 +32,25 @@ TEST_CASE("enum_helper_test - default to_string")
     std::string s;
     s += to_string(E::one) + " " + to_string(E::two);
     REQUIRE( s == "one two" );
+}
+
+TEST_CASE("enum_helper_test - from_string")
+{
+    REQUIRE( from_string<E>("one") == E::one );
+    REQUIRE( from_string<E>("two") == E::two );
+    REQUIRE( from_string<E>("four") == E::four );
+}
+
+TEST_CASE("enum_helper_test - istream")
+{
+    for (auto s : { std::make_tuple("one", E::one), std::make_tuple("two", E::two), std::make_tuple("four", E::four) } )
+    {
+        std::stringstream ss;
+        ss << std::get<0>(s);
+        E e;
+        ss >> e;
+        REQUIRE(e == std::get<1>(s));
+    }
 }
 
 TEST_CASE("enum_helper_test - default")
